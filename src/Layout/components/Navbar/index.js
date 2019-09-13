74585a15678872ea00c753a68ee92ee695be18c8
unknown <marcosiegman01@gmail.com>
2019-09-12 21:08:07 -0500

    fixed warnings

--- src/Layout/components/Navbar/index.js
+++ src/Layout/components/Navbar/index.js
@@ -1,29 +1,23 @@
-//Esta es la barra de navegación de las páginas.
-import React, { useState } from "react";
-import {
-  LoginButton,
-  SignupButton,
-  Header,
-  ButtonContainer,
-  MobileMenu
-} from "./styles";
-import { Logo } from "../../../global/styles/foundations/Logo";
+// Esta es la barra de navegación de las páginas.
+import React from 'react'
+import { LoginButton, SignupButton, Header, ButtonContainer } from './styles'
+import { Logo } from '../../../global/styles/foundations/Logo'
 
 export const Navbar = () => {
   // function handleClick(e) {
   //   const [ menuDisplayed, setMenuDisplayed ] = useState(false);
   // }
-  //The User Interface
+  // The User Interface
   return (
-    <Header className="nav-bar">
-      <Logo to="/">SIGN</Logo>
+    <Header className='nav-bar'>
+      <Logo to='/'>SIGN</Logo>
       <ButtonContainer>
-        <LoginButton to="/login">Iniciar sesión</LoginButton>
-        <SignupButton to="/signup">Registrarse</SignupButton>
+        <LoginButton to='/login'>Iniciar sesión</LoginButton>
+        <SignupButton to='/signup'>Registrarse</SignupButton>
       </ButtonContainer>
       {/* <MobileMenu onClick={handleClick}>menu</MobileMenu> */}
     </Header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
