848f2a0b3593b3fc8c67743f99cbc4844da768fd
Marco Macedo <marcosiegman01@gmail.com>
2019-09-20 17:14:15 -0500

    removed unnesesary styles from Login

--- src/Authentication/components/Login/styles.js
+++ src/Authentication/components/Login/styles.js
@@ -1,22 +1,25 @@
 //Components
 import styled from "styled-components";
 //Resources
-import LoginImage from "../../utils/images/LoginImage.png";
+import LoginImage from "../../../global/static/img/auth.svg";
 import { whiteColorDark } from "../../../global/styles/variables";
-import {
-  setHalfScreenContainer,
-  setChildContainer
-} from "../../../global/styles/foundations/Containers";
+import { appPadding } from "../../../global/styles/variables";
 export const Container = styled.div`
-  ${setHalfScreenContainer()};
-  /* position: relative; */
+  padding: ${appPadding};
+  padding-bottom: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  gap: 1rem;
   overflow: hidden;
 `;
 export const LoginForm = styled.div`
-  ${setChildContainer("2/-1", "1/2")};
+  grid-column: 1/2;
+  grid-row: 1/-1;
   display: flex;
   flex-direction: column;
-  padding-top: 12.8vh;
+  justify-content: flex-start;
+
   width: 80%;
   & .MuiButtonBase-root {
     width: 100%;
@@ -29,31 +32,29 @@ export const LoginForm = styled.div`
     width: 95%;
   }
   @media (max-width: 560px) {
-    ${setChildContainer("1/-1", "1/2")};
+    grid-column: 1/-1;
     padding: 20% 5%;
   }
 `;
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-
   margin-top: 1rem;
   box-sizing: border-box;
-  width: 70%;
-  @media (max-width: 1000px) {
-    width: 100%;
-  }
+  width: 100%;
 `;
 export const Picture = styled.picture`
-  ${setChildContainer("1/2", "1/2")};
+  grid-column: 2/-1;
+  grid-row: 1/-1;
+
   min-height: 100vh;
   width: 50vw;
   position: relative;
-  left: -12%;
+  opacity: 0.7;
   background-image: url(${LoginImage});
-  background-size: cover;
+  background-size: contain;
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: bottom;
 
   @media (max-width: 560px) {
     display: none;
