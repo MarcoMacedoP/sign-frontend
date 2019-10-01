f39036441b68ef2f071a458acbc7b303164d74ca
Marco Macedo <marcosiegman01@gmail.com>
2019-10-01 10:35:40 -0500

    fixed clients page bug

--- src/Clients/components/ClientsList/index.js
+++ src/Clients/components/ClientsList/index.js
@@ -1,26 +1,21 @@
 // Components
 import React from "react";
-import { Link } from "react-router-dom";
-import { LongCard, List } from "../../../global/components";
+import {Link} from "react-router-dom";
+import {LongCard, List} from "../../../global/components";
 
-// hooks
-import { useRedirect } from "../../../global/hooks/useRedirect";
 // styled-Components
-import { LongList } from "../../../global/styles/Lists";
+import {LongList} from "../../../global/styles/Lists";
+//utils
+import {CLIENTS_ROUTE} from "../../../global/utils/routes";
 
-export const ClientsList = ({ match }) => {
-  const { setRedirect, renderRedirect } = useRedirect("/app/clients/add");
-  const onClick = () => setRedirect(true);
-  return (
-    <List title="Clientes" onClick={onClick}>
-      <LongList>
-        {[1, 2, 3, 4].map(id => (
-          <Link to={`${match.path}${id}`} key={id}>
-            <LongCard />
-          </Link>
-        ))}
-      </LongList>
-      {renderRedirect()}
-    </List>
-  );
-};
+export const ClientsList = () => (
+  <List title="Clientes">
+    <LongList>
+      {[1, 2, 3, 4].map(id => (
+        <Link to={`${CLIENTS_ROUTE}${id}`} key={id}>
+          <LongCard />
+        </Link>
+      ))}
+    </LongList>
+  </List>
+);
