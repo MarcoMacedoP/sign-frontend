245a95c389e365a4d335ccbd95a01e84942d4122
Marco Macedo <marcosiegman01@gmail.com>
2019-10-18 11:58:01 -0500

    fixed styles on LateralMenu

--- src/global/styles/foundations/Logo.js
+++ src/global/styles/foundations/Logo.js
@@ -2,18 +2,18 @@ import {
   fontTitle,
   blackColorTransparent
 } from "../../styles/variables";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 import styled from "styled-components";
 
 export const Logo = styled(Link)`
-text-decoration: none;
-color: ${blackColorTransparent};
- font-size: 1.5rem;
+  text-decoration: none;
+  color: ${blackColorTransparent};
+  font-size: 1.5rem;
   font-weight: 500;
   font-family: ${fontTitle};
   text-transform: uppercase;
-   &:hover {
-    opacity: 0.60;
-}
+  &:hover {
+    opacity: 0.6;
+  }
 `;
