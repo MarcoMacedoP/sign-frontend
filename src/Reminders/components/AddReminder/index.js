bfa291bcd727e42a6bd5a62e0e06f3ced74ab28a
Marco Macedo <marcosiegman01@gmail.com>
2019-10-24 17:42:03 -0500

    added AddReminder component

---
+++ src/Reminders/components/AddReminder/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+//components
+import {Modal, AddPage, Input} from "../../../global/components/";
+//redux
+import {connect} from "react-redux";
+import {fetchAddReminder} from "../../../global/redux/actions/reminders";
+//hooks
+import {useState, useEffect} from "react";
+import {useHandleState} from "../../../global/hooks";
+function AddReminder({
+  loadingAddReminder,
+  errorOnAddReminder,
+  fetchAddReminder,
+  onClose,
+  isOpen
+}) {
+  //error handling
+  const [error, setError] = useState(null);
+  useEffect(() => setError(errorOnAddReminder), [errorOnAddReminder]);
+  const setErrorToNull = () => setError(null);
+  //state handling
+  const {state, addFormValueToState} = useHandleState({
+    title: "",
+    description: "",
+    date: ""
+  });
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <AddPage
+        title="Agregar un recordatorio."
+        aboutTitle="Sobre los recordatorios"
+        about="Los recordatorios son una excelente manera de recordar las cosas importantes."
+        isLoading={loadingAddReminder}
+        error={error}
+        onErrorClose={setErrorToNull}
+        onSubmit={fetchAddReminder}
+      >
+        <Input
+          value={state.title}
+          name="title"
+          label="Nombre del recordatorio"
+          onChange={addFormValueToState}
+        />
+        <Input
+          value={state.title}
+          name="description"
+          label="Agrega una descripción del recordatorio."
+          onChange={addFormValueToState}
+        />
+        <Input
+          value={state.title}
+          name="date"
+          type="date"
+          label="Fecha"
+          onChange={addFormValueToState}
+        />
+      </AddPage>
+    </Modal>
+  );
+}
+
+const mapStateToProps = ({reminders}) => ({
+  loadingAddReminder: reminders.status.loadingAddReminder,
+  errorOnAddReminder: reminders.status.errorOnAddReminder
+});
+
+export default connect(
+  mapStateToProps,
+  {fetchAddReminder}
+)(AddReminder);
