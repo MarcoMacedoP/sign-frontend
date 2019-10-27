979182affd230b323d4149b70397b82c69701dcf
Marco Macedo <marcosiegman01@gmail.com>
2019-10-27 13:30:29 -0600

    fixed layout dashboard

--- src/Dashboard/components/Dashboard/styles.js
+++ src/Dashboard/components/Dashboard/styles.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
-import {cellphoneMediaQuery} from "../../../global/styles/mediaQuerys";
+import {
+  cellphoneMediaQuery,
+  mediumScreen
+} from "../../../global/styles/mediaQuerys";
 // static
 import backgroundImage from "../../../global/static/img/background.svg";
 export const Container = styled.div`
@@ -11,17 +14,19 @@ export const Container = styled.div`
   position: relative;
 
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(2, minmax(300px, auto));
   column-gap: 2.5rem;
   row-gap: 2.5rem;
-  grid-template-rows: repeat(3, minmax(120px, 16vh));
+  grid-template-rows: repeat(3, minmax(min-content, auto));
 
   background-image: url(${backgroundImage});
   background-position: bottom;
   background-size: cover;
-
-  @media ${cellphoneMediaQuery} {
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  @media screen and (max-width: 900px) {
+    grid-template-columns: minmax(300px, 1fr);
     grid-template-rows: repeat(3, minmax(130px, 15vh));
+  }
+  @media ${cellphoneMediaQuery} {
+    grid-template-columns: minmax(250px, 1fr);
   }
 `;
