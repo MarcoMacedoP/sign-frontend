0f9ea1f946103caafa6cd553ad1fe062b3a55ee0
Marco Macedo <marcosiegman01@gmail.com>
2019-11-13 21:39:23 -0600

    updated App.js  with damaged .git

--- src/Test/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {AppContainer} from "../global/styles/Containers";
-export const Test = () => (
-  <AppContainer>
-    <h1>Esto es la pagina de testing </h1>
-  </AppContainer>
-);
