0c9f28b71f479da57b711082c176bd4fb8302715
marco <marcosiegman01@gmail.com>
2019-11-24 23:46:45 -0600

    removed autoComplete on inputs

--- src/global/components/Input/index.tsx
+++ src/global/components/Input/index.tsx
@@ -1,128 +1,132 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-  Label,
-  InputForm,
-  Container,
-  TextArea,
-  StyledErrorSpan
-} from "./styles";
+	Label,
+	InputForm,
+	Container,
+	TextArea,
+	StyledErrorSpan
+} from './styles';
 import {
-  EMAIL_VALIDATION,
-  DIGITS_VALIDATION,
-  MIN_8_CHARS_VALIDATION,
-  ESPECIAL_CHARS_VALIDATION,
-  CHARS_ALPHABET_VALIDATION,
-  TEXT_VALIDATION
-} from "../../utils/validations";
+	EMAIL_VALIDATION,
+	DIGITS_VALIDATION,
+	MIN_8_CHARS_VALIDATION,
+	ESPECIAL_CHARS_VALIDATION,
+	CHARS_ALPHABET_VALIDATION,
+	TEXT_VALIDATION
+} from '../../utils/validations';
 
-type inputType = "text" | "email" | "number" | "date" | "textarea" | "password";
+type inputType = 'text' | 'email' | 'number' | 'date' | 'textarea' | 'password';
 
 interface InputProps {
-  name: string;
-  label?: string;
-  placeholder?: string;
-  type?: inputType;
-  onChange: Function | any;
-  value: any;
-  setError?: Function | any;
-  displayError?: boolean;
+	name: string;
+	label?: string;
+	placeholder?: string;
+	type?: inputType;
+	onChange: Function | any;
+	value: any;
+	setError?: Function | any;
+	displayError?: boolean;
 }
 
 /** This hooks validates the user input, depending on the input type.
  *  Only updates the error if the user has clicked in the form.
  */
 const useErrorOnInput = ({ type, value }: { type: inputType; value: any }) => {
-  const [localError, setLocalError] = useState<null | string>(null);
-  const [userHasClicked, setUserHasClicked] = useState(false);
+	const [localError, setLocalError] = useState<null | string>(null);
+	const [userHasClicked, setUserHasClicked] = useState(false);
 
-  useEffect(() => {
-    if (userHasClicked) {
-      switch (type) {
-        case "date": {
-          value < new Date()
-            ? setLocalError(
-                "La fecha que intentas ingresar ya pasó, ¿Estás seguro de querer hacer esto?"
-              )
-            : setLocalError(null);
-          break;
-        }
-        case "number":
-          value.match(DIGITS_VALIDATION)
-            ? setLocalError(null)
-            : setLocalError("Debes ingresar números!");
-          break;
-        case "email":
-          value.match(EMAIL_VALIDATION)
-            ? setLocalError(null)
-            : setLocalError("El email no es válido");
-          break;
-        case "password":
-          value.match(DIGITS_VALIDATION) &&
-          value.match(MIN_8_CHARS_VALIDATION) &&
-          value.match(ESPECIAL_CHARS_VALIDATION) &&
-          value.match(CHARS_ALPHABET_VALIDATION)
-            ? setLocalError(null)
-            : setLocalError(
-                "Contraseña demasiado debil. Minimo debe contener 8 caracteres, un digito y algun carácter especial ($&%?¿!¡)"
-              );
-          break;
+	useEffect(() => {
+		if (userHasClicked) {
+			switch (type) {
+				case 'date': {
+					value < new Date()
+						? setLocalError(
+								'La fecha que intentas ingresar ya pasó, ¿Estás seguro de querer hacer esto?'
+						  )
+						: setLocalError(null);
+					break;
+				}
+				case 'number':
+					value.match(DIGITS_VALIDATION)
+						? setLocalError(null)
+						: setLocalError('Debes ingresar números!');
+					break;
+				case 'email':
+					value.match(EMAIL_VALIDATION)
+						? setLocalError(null)
+						: setLocalError('El email no es válido');
+					break;
+				case 'password':
+					value.match(DIGITS_VALIDATION) &&
+					value.match(MIN_8_CHARS_VALIDATION) &&
+					value.match(ESPECIAL_CHARS_VALIDATION) &&
+					value.match(CHARS_ALPHABET_VALIDATION)
+						? setLocalError(null)
+						: setLocalError(
+								'Contraseña demasiado debil. Minimo debe contener 8 caracteres, un digito y algun carácter especial ($&%?¿!¡)'
+						  );
+					break;
 
-        default:
-          value.match(TEXT_VALIDATION)
-            ? setLocalError(null)
-            : setLocalError("Texto demasiado corto/largo. ");
-      }
-    }
-  }, [value]);
+				default:
+					value.match(TEXT_VALIDATION)
+						? setLocalError(null)
+						: setLocalError('Texto demasiado corto/largo. ');
+			}
+		}
+	}, [value]);
 
-  return { error: localError, setUserHasClicked };
+	return { error: localError, setUserHasClicked };
 };
 
 export const Input: React.FC<InputProps> = ({
-  name,
-  label = "",
-  type = "text",
-  placeholder,
-  onChange,
-  value,
-  displayError = true
+	name,
+	label = '',
+	type = 'text',
+	placeholder,
+	onChange,
+	value,
+	displayError = true
 }) => {
-  const [active, setActive] = useState(false);
-  const { error, setUserHasClicked } = useErrorOnInput({ type, value });
+	const [active, setActive] = useState(false);
+	const { error, setUserHasClicked } = useErrorOnInput({ type, value });
 
-  const handleBlur = () => setActive(false);
-  const handleFocus = () => {
-    setUserHasClicked(true);
-    setActive(true);
-  };
+	const handleBlur = () => setActive(false);
+	const handleFocus = () => {
+		setUserHasClicked(true);
+		setActive(true);
+	};
 
-  return (
-    <Container onFocus={handleFocus} onBlur={handleBlur}>
-      <Label htmlFor={name} active={active}>
-        {label || name}
-      </Label>
-      {type === "textarea" ? (
-        <TextArea
-          name={name}
-          placeholder={placeholder}
-          active={active}
-          onChange={onChange}
-          value={value}
-          error={displayError && error}
-        />
-      ) : (
-        <InputForm
-          id={name}
-          type={type}
-          placeholder={placeholder}
-          name={name}
-          active={active}
-          onChange={onChange}
-          value={value}
-          error={displayError && error}
-        />
-      )}
-      {displayError && error && <StyledErrorSpan> {error} </StyledErrorSpan>}
-    </Container>
-  );
+	return (
+		<Container onFocus={handleFocus} onBlur={handleBlur}>
+			<Label htmlFor={name} active={active}>
+				{label || name}
+			</Label>
+			{type === 'textarea' ? (
+				<TextArea
+					name={name}
+					placeholder={placeholder}
+					active={active}
+					onChange={onChange}
+					value={value}
+					error={displayError && error}
+					autoComplete="off"
+				/>
+			) : (
+				<InputForm
+					id={name}
+					type={type}
+					placeholder={placeholder}
+					name={name}
+					active={active}
+					onChange={onChange}
+					value={value}
+					error={displayError && error}
+					autoComplete="off"
+				/>
+			)}
+			{displayError && error && (
+				<StyledErrorSpan> {error} </StyledErrorSpan>
+			)}
+		</Container>
+	);
 };
--- src/global/components/Input/styles.js
+++ src/global/components/Input/styles.js
@@ -1,49 +1,49 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 import {
-  blackColorTransparent,
-  fontBody,
-  whiteColorDark,
-  errorColorLigth,
-  mainColor
-} from "../../styles/variables";
+	blackColorTransparent,
+	fontBody,
+	whiteColorDark,
+	errorColorLigth,
+	mainColor
+} from '../../styles/variables';
 
 export const BaseInputStyles = () => css`
-  border: ${props =>
-    props.active
-      ? `4px solid ${mainColor}`
-      : `1px solid ${blackColorTransparent}`};
-  ${props => (props.error ? `border-color:${errorColorLigth};` : null)};
-  transition: border 200ms linear;
+	border: ${(props) =>
+		props.active
+			? `4px solid ${mainColor}`
+			: `1px solid ${blackColorTransparent}`};
+	${(props) => (props.error ? `border-color:${errorColorLigth};` : null)};
+	transition: border 200ms linear;
 
-  border-top: none;
-  border-right: none;
-  border-left: none;
-  margin: ${props => (props.error ? "0" : "0.5rem 0")};
-  padding: 0 0.5rem 0.5rem 0;
-  background-color: transparent;
-  font-family: ${fontBody};
-  font-size: 1rem;
-  width: 100%;
+	border-top: none;
+	border-right: none;
+	border-left: none;
+	margin: ${(props) => (props.error ? '0' : '0.5rem 0')};
+	padding: 0 0.5rem 0.5rem 0;
+	background-color: transparent;
+	font-family: ${fontBody};
+	font-size: 1rem;
+	width: 100%;
 `;
 
 export const Container = styled.div`
-  width: 100%;
+	width: 100%;
 `;
 export const StyledErrorSpan = styled.span`
-  font-size: 0.85rem;
-  color: ${errorColorLigth};
-  margin: 0.25rem 0 0.5rem 0;
-  display: block;
+	font-size: 0.85rem;
+	color: ${errorColorLigth};
+	margin: 0.25rem 0 0.5rem 0;
+	display: block;
 `;
 export const Label = styled.label`
-  font-size: ${props => (props.active ? "12px" : "1rem")};
-  color: ${props => (props.active ? mainColor : whiteColorDark)};
+	font-size: ${(props) => (props.active ? '12px' : '1rem')};
+	color: ${(props) => (props.active ? mainColor : whiteColorDark)};
 
-  transition: color 200ms linear, font-size 200ms linear;
+	transition: color 200ms linear, font-size 200ms linear;
 `;
 export const InputForm = styled.input`
-  ${BaseInputStyles()}
+	${BaseInputStyles()}
 `;
 export const TextArea = styled.textarea`
-  ${BaseInputStyles()}
+	${BaseInputStyles()}
 `;
